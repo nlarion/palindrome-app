e74a6f355fca60403d093f70e4da9361eaa0f895
Neil Larion and Tim Durward <neil.larion@gmail.com>
2016-01-20 14:49:55 -0800

    refactor javascript

--- js/master.js
+++ js/master.js
@@ -8,44 +8,24 @@ function oddEven(string){
 }
 
 var stripText = function(message){
-  message = message.toLowerCase();
-  message = message.replace(/ /g,'');
-  message = message.replace(/[^\w\s]/gi, '');
-  return message;
+  return message.toLowerCase().replace(/ /g,'').replace(/[^\w\s]/gi, '');
 };
 
 function palindrome(string){
   string = stripText(string);
+  var splitInt = string.length/2;
   if(oddEven(string)){
-    var splitInt = string.length/2;
     var lastPart = string.slice(splitInt);
-    lastPart = lastPart.split("");
-    lastPart = lastPart.reverse();
-    lastPart = lastPart.join('');
-    var firstPart = string.slice(0,splitInt);
-    console.log("first part: "+ firstPart +" lastPart: " +lastPart)
-    if(lastPart==firstPart){
-      return true;
-    } else {
-      return false;
-    }
   }else{
-    console.log("else statement");
-    var splitInt = string.length/2;
-    // console.log(splitInt);
     splitInt = Math.floor(splitInt);
-    console.log(splitInt);
     var lastPart = string.slice(splitInt+1);
-    lastPart = lastPart.split("");
-    lastPart = lastPart.reverse();
-    lastPart = lastPart.join('');
+  }
+    lastPart = lastPart.split("").reverse().join('');
     var firstPart = string.slice(0,splitInt);
     console.log("first part: "+ firstPart +" lastPart: " +lastPart)
     if(lastPart==firstPart){
-      console.log("true");
       return true;
     } else {
       return false;
     }
-  }
 }
